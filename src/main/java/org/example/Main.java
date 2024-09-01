b268d60b1e2680ec9d205c7e018a4d798206ad88
elridge1311 <elridgefernandes11@gmail.com>
2024-09-01 15:38:31 +0530

    updated the print statement

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello and welcome!");
+        System.out.println("This is basic maven project integrated with jenkins");
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
