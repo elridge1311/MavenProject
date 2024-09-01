91a206cea64c35107b628265e43cbe48d0e4bbfc
elridge1311 <elridgefernandes11@gmail.com>
2024-09-01 20:42:11 +0530

    updating main file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,29 @@
 package org.example;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class Main {
-    private static Properties properties = new Properties();
-
     public static void main(String[] args) throws IOException {
-        InputStream input = new FileInputStream(".\\target\\config\\config.properties");
-        properties.load(input);
-        String myProperty = properties.getProperty("my.property");
-        System.out.println("My Name is : " + myProperty);
+        Properties properties = new Properties();
+        try (InputStream input = Main.class.getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                System.out.println("Sorry, unable to find config/config.properties");
+                System.exit(1);
+            }
+            properties.load(input);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            System.exit(1);
+        }
+
+        // Get the property value from the loaded properties file
+        String myProperty = properties.getProperty("my.property", "default value");
+
+        // Print the result
+        String template = "My Name is : ${my.property}";
+        String result = template.replace("${my.property}", myProperty);
+        System.out.println(result);
     }
 }
