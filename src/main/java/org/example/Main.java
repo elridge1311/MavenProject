169584e58d2e3958aaabe0897e377b19a78aed89
elridge1311 <elridgefernandes11@gmail.com>
2024-09-01 18:06:47 +0530

    parameterized name

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,18 @@
 package org.example;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class Main {
-    public static void main(String[] args) {
+    private static Properties properties = new Properties();
 
-        System.out.println("This is basic maven project integrated with jenkins");
+    public static void main(String[] args) throws IOException {
+        InputStream input = new FileInputStream("target/config/config.properties");
+        properties.load(input);
+        String myProperty = properties.getProperty("my.property");
+        System.out.println("My Name is : " + myProperty);
     }
 }
