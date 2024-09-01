37489a7332139b15046bcd85b3b59b9698bb2acb
elridge1311 <elridgefernandes11@gmail.com>
2024-09-01 15:39:54 +0530

    updated main file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,9 +5,5 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("This is basic maven project integrated with jenkins");
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
     }
 }
